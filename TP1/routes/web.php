a2fe343b9e7ebc463eb890004d5c537bf8cc4c53
Jules VINET LATRILLE <julesvinet64@gmail.com>
2025-02-12 15:38:08 +0100

    TP2 fait

--- TP1/routes/web.php
+++ TP1/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,19 @@ Route::get('/', function () {
 use App\Http\Controllers\ClientController;
 
 Route::get('/clients', ['as' => 'clients.index', 'uses' => 'ClientController@index']);
+
+Route::get('/clients/create', ['as' => 'clients.create', 'uses' => 'ClientController@create']);
+
+Route::post('/clients', ['as' => 'clients.store', 'uses' => 'ClientController@store']);
+
+Route::get('/clients/{id}', ['as' => 'clients.show', 'uses' => 'ClientController@show']);
+
+Route::get('/clients/{id}/edit', ['as' => 'clients.edit', 'uses' => 'ClientController@edit']);
+
+Route::put('/clients/{id}', ['as' => 'clients.update', 'uses' => 'ClientController@update']);
+
+Route::delete('/clients/{id}', ['as' => 'clients.destroy', 'uses' => 'ClientController@destroy']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
